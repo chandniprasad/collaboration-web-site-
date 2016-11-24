fa8b3d31624bcc098eed8f3e640b7a4ad6dc214a
chandniprasad <chandnimadakath@gmail.com>
2016-11-24 10:13:19 +0530

    Security is implemented.
    Security is implemented.

---
+++ CollaborationBackEnd2/src/main/java/com/niit/collaboration/model/JobApplication.java
@@ -0,0 +1,69 @@
+package com.niit.collaboration.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Table(name="c_jobapplication")
+public class JobApplication {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String userID;
+	private int jobID;
+    private Date dateApplied;
+    private String remarks;
+    private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public int getJobID() {
+		return jobID;
+	}
+	public void setJobID(int jobID) {
+		this.jobID = jobID;
+	}
+	public Date getDateApplied() {
+		return dateApplied;
+	}
+	public void setDateApplied(Date dateApplied) {
+		this.dateApplied = dateApplied;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+    
+	
+	
+    
+}
