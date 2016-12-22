c5776f661bf184c44e29a7d9f704293e215cbd93
Chandni Prasad <chandnimadakath@gmail.com>
2016-12-22 15:15:40 +0530

    Final collaboration backend project.
    Final collaboration backend project.

---
+++ CollaborationBackEnd-final/src/main/java/com/niit/collaboration/Controller/EventController.java
@@ -0,0 +1,15 @@
+package com.niit.collaboration.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventController {
+
+	 private static final Logger Logger = LoggerFactory.getLogger(UserController.class);
+	/*@Autowired
+	EventDAO eventDAO;
+	
+	@Autowired
+	 */
+}
